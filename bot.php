a093587ead5192f806c7a93b7be6ec38284d84b0
overpik <overpik@gmail.com>
2017-07-11 14:15:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,14 +12,14 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['text']+'lintbot';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Lint Bot'+$text
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
