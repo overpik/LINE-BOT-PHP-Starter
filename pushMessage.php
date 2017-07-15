74973a2447b9ed560394d4e0adf10c25303ce5e7
overpik <overpik@gmail.com>
2017-07-15 09:57:10 +0700

    Update pushMessage.php

--- pushMessage.php
+++ pushMessage.php
@@ -1,4 +1,12 @@
-
 <?php
-echo "Yes"
+$access_token = 'ifKSrOv1jXoNE12N1uKII0QOKqWrBSinwdXnnwUzclDMOCTqrJby6QHr15tdniiJmBptSk5EKSbD4v+HJos9Tlz5kZMpl5X6HRGLn5RG6uq2U6BUgw2nJHPQkeH5eBLVlJOWqoeX0CH763WNwPKPBAdB04t89/1O/w1cDnyilFU=';
+$channel = '926e169d43a233be864a63e84300a9c8';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage($access_token, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 ?>
