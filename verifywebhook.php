9c4578f61eb33f754294e621c31303ea4894c7f3
overpik <overpik@gmail.com>
2017-07-15 13:39:56 +0700

    Update verifywebhook.php

--- verifywebhook.php
+++ verifywebhook.php
@@ -12,6 +12,9 @@ $input = json_decode(file_get_contents('php://input'), true);
 
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
+if($message == 'hi'){
+
+  
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
@@ -39,4 +42,4 @@ if ($err) {
   echo $response;
 }
 
-
+}else
