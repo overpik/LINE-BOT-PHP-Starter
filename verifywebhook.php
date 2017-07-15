ebeee009c57bdd34bdca75d11046af51bf83b1d1
overpik <overpik@gmail.com>
2017-07-15 11:00:13 +0700

    Create verifywebhook.php

---
+++ verifywebhook.php
@@ -0,0 +1,11 @@
+<?php
+
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'abc123') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
