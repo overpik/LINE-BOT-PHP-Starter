7d2bfc3e302603cd9b3c69895c79d0f8575a2210
overpik <overpik@gmail.com>
2017-07-15 10:48:46 +0700

    Update pushnotify.php

--- pushnotify.php
+++ pushnotify.php
@@ -1,20 +1,21 @@
-define(‘LINE_API’,”https://notify-api.line.me/api/notify");
+<?
+define('LINE_API',"https://notify-api.line.me/api/notify");
  
-$token = “”; //ใส่Token ที่copy เอาไว้
-$str = “Hello”; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+$token = "i2PTsV6YCSc7bKt8twJbBoRFFQ2oQ1Qc54drlrH8qfI"; //ใส่Token ที่copy เอาไว้
+$str = "แจ้งเตือน มีคน Inbox Facebook"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
  
 $res = notify_message($str,$token);
 print_r($res);
 function notify_message($message,$token){
- $queryData = array(‘message’ => $message);
- $queryData = http_build_query($queryData,’’,’&’);
+ $queryData = array('message' => $message);
+ $queryData = http_build_query($queryData,'','&');
  $headerOptions = array( 
-         ‘http’=>array(
-            ‘method’=>’POST’,
-            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
-                      .”Authorization: Bearer “.$token.”\r\n”
-                      .”Content-Length: “.strlen($queryData).”\r\n”,
-            ‘content’ => $queryData
+         'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                      ."Authorization: Bearer ".$token."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
          ),
  );
  $context = stream_context_create($headerOptions);
@@ -22,3 +23,4 @@ function notify_message($message,$token){
  $res = json_decode($result);
  return $res;
 }
+?>
